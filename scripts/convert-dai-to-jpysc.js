c07db8c43ac163413d4f4823ad4c9ade574f43cc
pad01g <101613202+pad01g@users.noreply.github.com>
2022-12-05 02:20:28 +0000

    replace dsr with jsr

--- scripts/convert-dai-to-jpysc.js
+++ scripts/convert-dai-to-jpysc.js
@@ -1,7 +1,8 @@
 const { readFileSync, readdirSync, writeFileSync } = require("fs")
 const main = async () => {
     const languages = ["af", "de", "en", "es", "fa", "fr", "ga", "id", "it", "jp", "kr", "pl", "pt-br", "ro", "ru", "tr", "uk", "zh-TW"]
-    const relace_keys = ["daistats.dai_brewing", "daistats.dai_from_token", "daistats.dai_in_dsr", "daistats.dai_savings_rate", "daistats.dai_surplus_auctions", "daistats.debt_available_heal", "daistats.token_ceiling"]
+    const daiReplaceKeys = ["daistats.dai_brewing", "daistats.dai_from_token", "daistats.dai_in_dsr", "daistats.dai_savings_rate", "daistats.dai_surplus_auctions", "daistats.debt_available_heal", "daistats.token_ceiling"]
+    const dsrReplaceKeys = ["daistats.dai_in_dsr", "daistats.pie_in_dsr"]
     const defaultLang = "en"
     const files = readdirSync("./" + defaultLang)
     // read other languages
@@ -10,9 +11,14 @@ const main = async () => {
             const languageFilePath = "./" + language + "/" + file
             const languageFile = readFileSync(languageFilePath)
             const languageJson = JSON.parse(languageFile.toString("utf8"))
-            for (const key of relace_keys) {
+            for (const key of daiReplaceKeys) {
                 if (languageJson[key]) {
                     languageJson[key] = languageJson[key].replace(/dai/ig, "JPYSC")
+                }
+            }
+            for (const key of dsrReplaceKeys) {
+                if (languageJson[key]) {
+                    languageJson[key] = languageJson[key].replace(/dsr/ig, "JSR")
                 }
             }
             writeFileSync(languageFilePath, JSON.stringify(languageJson, null, 2))
