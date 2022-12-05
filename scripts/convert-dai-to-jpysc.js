72ba8fdbdafa658c35771b2139310c5097d90f1a
pad01g <101613202+pad01g@users.noreply.github.com>
2022-12-05 02:27:13 +0000

    convert mkr to yendao

--- scripts/convert-dai-to-jpysc.js
+++ scripts/convert-dai-to-jpysc.js
@@ -1,8 +1,23 @@
 const { readFileSync, readdirSync, writeFileSync } = require("fs")
 const main = async () => {
     const languages = ["af", "de", "en", "es", "fa", "fr", "ga", "id", "it", "jp", "kr", "pl", "pt-br", "ro", "ru", "tr", "uk", "zh-TW"]
-    const daiReplaceKeys = ["daistats.dai_brewing", "daistats.dai_from_token", "daistats.dai_in_dsr", "daistats.dai_savings_rate", "daistats.dai_surplus_auctions", "daistats.debt_available_heal", "daistats.token_ceiling"]
-    const dsrReplaceKeys = ["daistats.dai_in_dsr", "daistats.pie_in_dsr"]
+    const replaceList = [
+        {
+            replaceKeys: ["daistats.dai_brewing", "daistats.dai_from_token", "daistats.dai_in_dsr", "daistats.dai_savings_rate", "daistats.dai_surplus_auctions", "daistats.debt_available_heal", "daistats.token_ceiling"],
+            replaceRegex: /dai/ig,
+            replaceString: "JPYSC"
+        },
+        {
+            replaceKeys: ["daistats.dai_in_dsr", "daistats.pie_in_dsr"],
+            replaceRegex: /dsr/ig,
+            replaceString: "JSR"
+        },
+        {
+            replaceKeys: ["daistats.annual_mkr_burn_rate"],
+            replaceRegex: /mkr/ig,
+            replaceString: "YENDAO"
+        }
+    ]
     const defaultLang = "en"
     const files = readdirSync("./" + defaultLang)
     // read other languages
@@ -11,14 +26,11 @@ const main = async () => {
             const languageFilePath = "./" + language + "/" + file
             const languageFile = readFileSync(languageFilePath)
             const languageJson = JSON.parse(languageFile.toString("utf8"))
-            for (const key of daiReplaceKeys) {
-                if (languageJson[key]) {
-                    languageJson[key] = languageJson[key].replace(/dai/ig, "JPYSC")
-                }
-            }
-            for (const key of dsrReplaceKeys) {
-                if (languageJson[key]) {
-                    languageJson[key] = languageJson[key].replace(/dsr/ig, "JSR")
+            for (const replaceElement of replaceList) {
+                for (const key of replaceElement.replaceKeys) {
+                    if (languageJson[key]) {
+                        languageJson[key] = languageJson[key].replace(replaceElement.replaceRegex, replaceElement.replaceString)
+                    }
                 }
             }
             writeFileSync(languageFilePath, JSON.stringify(languageJson, null, 2))
